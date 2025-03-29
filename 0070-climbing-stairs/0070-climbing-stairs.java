c33fe9ce641422676ba61e4aea3e2a83d4255b9b
Priyansh Jain <priyanshjain8554@gmail.com>
2025-03-29 12:48:28 +0530

    Time: 0 ms (100%), Space: 40.7 MB (17.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,34 @@
 class Solution {
-    public int climbStairs(int n) {
-        int [] dp = new int[n+1];
+    public int helper(int [] memo, int n){
+        if( n == 0) return 1;
 
-        dp[0] = 1;
-
-        for(int i=1; i<dp.length; i++){
-            if( i == 1){
-                dp[i] = dp[i-1] + 0;
-                continue;
-            }
-
-            dp[i] = dp[i-1] + dp[i-2];
+        if(memo[n] != 0){
+            return memo[n];
         }
 
-        return dp[n];
+        memo[n] = helper(memo, n-1) + helper(memo, n-2);
+        return memo[n];
+
+
+    }
+    public int climbStairs(int n) {
+        // int [] dp = new int[n+1];
+
+        // dp[0] = 1;
+
+        // for(int i=1; i<dp.length; i++){
+        //     if( i == 1){
+        //         dp[i] = dp[i-1] + 0;
+        //         continue;
+        //     }
+
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+
+        // return dp[n];
+        int [] memo = new int[n+1];
+        memo[0] = 1;
+        memo[1] = 1;
+        return helper(memo, n);
     }
 }
