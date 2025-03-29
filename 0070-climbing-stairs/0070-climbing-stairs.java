f2b81861f4a2de62d4d8f9ed6f7a37fab62e5cef
Priyansh Jain <priyanshjain8554@gmail.com>
2025-03-29 12:43:29 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.27%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int [] dp = new int[n+1];
+
+        dp[0] = 1;
+
+        for(int i=1; i<dp.length; i++){
+            if( i == 1){
+                dp[i] = dp[i-1] + 0;
+                continue;
+            }
+
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
