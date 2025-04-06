cd237d8f6e01038ad4fa58c0c713863aa12da88a
Priyansh Jain <priyanshjain8554@gmail.com>
2025-04-06 16:25:29 +0530

    Time: 0 ms (100%), Space: 41.3 MB (51.18%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,44 @@
 class Solution {
     public boolean check(int[] nums) {
-        int [] arr = new int[nums.length];
-        int x= 0;
-        boolean flag = true;
+        // int [] arr = new int[nums.length];
+        // int x= 0;
+        // boolean flag = true;
 
-        while(x<nums.length){
-            for(int i=0; i<nums.length; i++){
-                arr[i] = nums[(i+x)% nums.length];
-            }
+        // while(x<nums.length){
+        //     // filling the elements from the nums to arr
+        //     for(int i=0; i<nums.length; i++){
+        //         arr[i] = nums[(i+x)% nums.length];
+        //     }
 
-            for(int i=1; i<arr.length; i++){
-                if(arr[i]<arr[i-1]){
-                    flag = false;
-                    break;
-                }
-            }
+        //     // checking if the arr is sorted or not
+        //     for(int i=1; i<arr.length; i++){
+        //         if(arr[i]<arr[i-1]){
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
 
-            if(flag == true){
-                return true;
-            }else{
-                flag = true;
+        //     if(flag == true){
+        //         return true;
+        //     }else{
+        //         flag = true;
+        //     }
+        //     x++;
+        // }
+        // return false;
+
+        int count = 0;
+        for(int i=0 ; i<nums.length; i++){
+            if(nums[i] > nums[(i+1) % nums.length]){
+                count++;
             }
-            x++;
         }
-        return false;
+
+        if(count >1){
+            return false;
+        }else{
+            return true;
+        }
         }
         
     }
