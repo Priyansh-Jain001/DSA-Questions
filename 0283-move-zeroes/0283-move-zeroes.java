8550ee6334bb966484cb79f4e299415e35acc16f
Priyansh Jain <priyanshjain8554@gmail.com>
2025-04-06 16:50:36 +0530

    Time: 2 ms (84.94%), Space: 45.9 MB (80.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int lastNonZeroIdx = 0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]!=0){
-        //         nums[lastNonZeroIdx]  = nums[i];
-        //         lastNonZeroIdx++;
-        //     }
-        // }
-        // for(int i= lastNonZeroIdx; i<nums.length; i++){
-        //     nums[i] = 0;
-        // }
+        /* Brute Force approach
+        count the no. of zeros and fill the array accordingly
+        */
 
+        int lastNonZeroIdx = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[lastNonZeroIdx]  = nums[i];
+                lastNonZeroIdx++;
+            }
+        }
+        for(int i= lastNonZeroIdx; i<nums.length; i++){
+            nums[i] = 0;
+        }
+
+/* Approach 2 (Two-pointer)
        for (int i = 0; i < nums.length; i++) {
         if (nums[i] != 0) {
-            // Swap elements if the current index is different from lastNonZeroIdx
+            
+    Swap elements if the current index is different from lastNonZeroIdx
             if (i != lastNonZeroIdx) {
                 int temp = nums[i];
                 nums[i] = nums[lastNonZeroIdx];
@@ -22,5 +28,6 @@ class Solution {
             lastNonZeroIdx++;
         }
     }
+*/
     }
 }
