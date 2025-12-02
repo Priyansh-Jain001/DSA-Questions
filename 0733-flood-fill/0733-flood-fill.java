3c091dc9abad8d68a63d5567fbede4ed05a01f46
Priyansh Jain <priyanshjain8554@gmail.com>
2025-12-03 03:54:34 +0530

    Time: 1 ms (44.42%), Space: 47 MB (9.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,26 +6,26 @@ class Solution {
         image[sr][sc] = color;
         q.add(new int[] {sr, sc});
         
+        // Track whose color is changed
         boolean [][] visited = new boolean[image.length][image[0].length];
         visited[sr][sc] = true;
 
-        int [] dx = {1,-1,0,0};
-        int [] dy = {0,0,1,-1};
+        int [] dx = {1,-1,0,0}; // move up and down
+        int [] dy = {0,0,1,-1}; // move left and right
 
         while(!q.isEmpty()){
             int [] curr = q.remove();
 
             int x = curr[0], y = curr[1];
+            visited[x][y] = true;
 
             for(int d=0 ; d<4; d++){
-                int nx = x+dx[d];
-                int ny = y+dy[d];
-
-
+                int nx = x+dx[d]; // new x co-ordinate to check for
+                int ny = y+dy[d]; // new y co-ordinate to check for
 
                 if(nx>=0 && ny>=0 && nx<image.length && ny<image[0].length && image[nx][ny] == pixel && !visited[nx][ny]){
                     image[nx][ny] = color;
-                    visited[nx][ny] = true;
+                    
                     q.add(new int[] {nx, ny});
                 }
             }
