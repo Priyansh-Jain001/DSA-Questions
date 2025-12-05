9fcb04b200001e8c8e03185abf50df372bc8c23e
Priyansh Jain <priyanshjain8554@gmail.com>
2025-12-06 00:03:33 +0530

    Time: 40 ms (5.45%), Space: 46.9 MB (30.38%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+         // store the sorted order in ans array
+        int [] ans = new int[numCourses]; int k =0;  // help iterate over the answer array
+
+        
+        if(prerequisites.length == 0){
+            for(int i=0; i<numCourses; i++){
+                ans[i] = i;
+            }
+        }
+
+       
+        int [] in_deg = new int[numCourses];
+        
+        // fills the in_def with 0 initially
+        Arrays.fill(in_deg, 0);
+
+        // Finding the indegree of every couse
+        for(int i=0; i<prerequisites.length; i++){
+            in_deg[prerequisites[i][0]]++;
+        }
+
+        for(int i: in_deg){
+            System.out.print(i+" ");
+        }
+
+        // adding the 0 indegree(non-dependent node) in the queue 
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<in_deg.length; i++){
+            if(in_deg[i] == 0){
+                q.add(i);
+            }
+        }
+
+        
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            ans[k] = curr;
+            k++;
+
+            for(int i=0; i<prerequisites.length; i++){
+                if(prerequisites[i][1] == curr){
+                    
+                    // neighbour in_deg decrement
+                    in_deg[prerequisites[i][0]]--;
+
+                    if(in_deg[prerequisites[i][0]] == 0){
+                        q.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+
+        for(int i: in_deg){
+            if(i != 0){
+                return new int[]{};
+            }
+        }
+
+        return ans;
+    }
+}
